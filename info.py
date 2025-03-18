c2b1770323e8541ad894e9b94db9656b06a7eaf0
hostkimjang <37247073+hostkimjang@users.noreply.github.com>
2025-03-18 23:31:25 +0900

    Update info.py

--- info.py
+++ info.py
@@ -1,56 +1,70 @@
 class NovelInfo:
-    def __init__(self, platform, title, info, author, agegrade, score, new_status, content_type, locate, thumbnail):
+    def __init__(self, platform, id, title, info, author, chapter, agegrade, score, new_status, content_type, locate, thumbnail, last_update):
         self.platform = platform
+        self.id = id
         self.title = title
         self.info = info
         self.author = author
+        self.chapter = chapter
         self.agegrade = agegrade
         self.score = score
         self.new_status = new_status
         self.content_type = content_type
         self.locate = locate
         self.thumbnail = thumbnail
+        self.last_update = last_update
 
     def __str__(self, *args, **kwargs):
         return (f"platform: {self.platform}\n"
+                f"id: {self.id}\n"
                 f"title: {self.title}\n"
                 f"info: {self.info}\n"
                 f"author: {self.author}\n"
+                f"chapter: {self.chapter}\n"
                 f"agegrade: {self.agegrade}\n"
                 f"score: {self.score}\n"
                 f"new_status: {self.new_status}\n"
                 f"content_type: {self.content_type}\n"
                 f"locate: {self.locate}\n"
-                f"thumbnail: {self.thumbnail}\n")
+                f"thumbnail: {self.thumbnail}\n"
+                f"last_update: {self.last_update}\n"
+                )
+
 
     def to_dict(self):
         return {
             "platform": self.platform,
+            "id": self.id,
             "title": self.title,
             "info": self.info,
             "author": self.author,
+            "chapter": self.chapter,
             "agegrade": self.agegrade,
             "score": self.score,
             "new_status": self.new_status,
             "content_type": self.content_type,
             "locate": self.locate,
-            "thumbnail": self.thumbnail
+            "thumbnail": self.thumbnail,
+            "last_update": self.last_update
         }
 
 
 
 
-def set_novel_info(platform, title, info, author, agegrade, score, new_status, content_type, locate, thumbnail):
+def set_novel_info(platform, id, title, info, author, chapter, agegrade, score, new_status, content_type, locate, thumbnail, last_update):
     print("-" * 100)
     print(f"platform: {platform}")
+    print(f"id: {id}")
     print(f"title: {title}")
     print(f"info: {info}")
     print(f"author: {author}")
+    print(f"chapter: {chapter}")
     print(f"grade: {agegrade}")
     print(f"score: {score}")
     print(f"new_status: {new_status}")
     print(f"content_type: {content_type}")
     print(f"thumbnail: {thumbnail}")
+    print(f"last_update: {last_update}")
     print(f"locate: {locate}")
     print("-" * 100)
-    return NovelInfo(platform, title, info, author, agegrade, score, new_status, content_type, locate, thumbnail)
+    return NovelInfo(platform, id, title, info, author, chapter, agegrade, score, new_status, content_type, locate, thumbnail, last_update)
