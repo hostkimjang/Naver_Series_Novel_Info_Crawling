4945f8f6259f2520916dcbe95e9ed5ddca863d6b
kimjang <37247073+hostkimjang@users.noreply.github.com>
2025-06-26 22:11:34 +0900

    Update naver_api.py

--- naver_api.py
+++ naver_api.py
@@ -3,7 +3,6 @@ import concurrent.futures
 import hashlib
 import hmac
 import os
-import pprint
 import time
 import urllib.parse
 import chromedriver_autoinstall
@@ -11,11 +10,9 @@ import requests
 import threading
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
 from store import store_final
 
 max_worker = 50
